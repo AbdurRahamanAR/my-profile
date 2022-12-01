4cf15136201fb272011a938afe284eb96e96738d
Abdur Rahaman <abdur@kimp.xyz>
2022-12-02 02:26:45 +0600

    add dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://abdurrahaman.com",
+  integrations: [mdx(), sitemap(), tailwind(), react()],
 });
